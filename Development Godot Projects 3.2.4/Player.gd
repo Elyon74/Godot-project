408b7e42a291d4a56bdffd0a2849b3cdd3f48b02
Elyon74 <48656046+Elyon74@users.noreply.github.com>
2021-01-28 00:27:50 +0100

    1.0

--- Development Godot Projects 3.2.4/Player.gd
+++ Development Godot Projects 3.2.4/Player.gd
@@ -1,16 +1,10 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+# Apellez une fois
 func _ready():
-	pass # Replace with function body.
+	var Hp = 3
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Apellez a toute les frames
+func _process(delta):
+	pass
